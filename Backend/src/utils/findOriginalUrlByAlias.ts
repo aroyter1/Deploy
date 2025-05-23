c4561dcfef8ffbe7a1477bf200d805610a52e65e
nedemo2025 <ambersticky@indigobook.com>
2025-05-23 14:09:20 +0500

    !

--- Backend/src/utils/findOriginalUrlByAlias.ts
+++ Backend/src/utils/findOriginalUrlByAlias.ts
@@ -1,5 +1,5 @@
 import UrlModel from '../models/Url';
-
+ 
 export default async function findOriginalUrlByAlias(alias: string): Promise<string | null> {
   const doc = await UrlModel.findOne({ alias });
   return doc ? doc.originalUrl : null;
